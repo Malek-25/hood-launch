e5aa450eb34a821fbc78498c73546142d3310024
Malek-25 <malekkh136@gmail.com>
2026-07-22 16:00:25 +0300

    config: set deployed factory address on Robinhood Chain mainnet

--- app/config.js
+++ app/config.js
@@ -1,3 +1,2 @@
-// Set this once after deploying HoodieTokenLauncherLauncher on Robinhood Chain.
-// Regular users never see or enter this address.
-window.HOODIEPAD_FACTORY_ADDRESS = "";
+// Deployed HoodieTokenLauncherLauncher on Robinhood Chain mainnet (chain ID 4663).
+window.HOODIEPAD_FACTORY_ADDRESS = "0x13a508274C010eE40A1EB06F3cb17a5641716E36";
